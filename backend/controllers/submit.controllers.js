36b3ee0a70b77ce20520660150341bab1e334b9b
Divyanshu5194 <d40737865@gmail.com>
2026-02-19 22:13:22 +0530

    added the run code functionality and fioxed the submit code function

--- backend/controllers/submit.controllers.js
+++ backend/controllers/submit.controllers.js
@@ -1,25 +1,47 @@
 import Problems from "../models/problems.models.js"
 import Submissions from "../models/submissions.models.js"
 import TestCases from "../models/testcases.models.js"
+import { User } from "../models/users.model.js"
 import { submitToken } from "../utils/submitBatch.utils.js"
 import { verifyTestCases } from "../utils/TestcaseValidators.js"
 
-export const submit=async (req,res)=>{
+const submit=async (req,res)=>{
     try{
         const {user} =req
         const {_id:userId}=user
-        const {problemId,solution}=req.body
-        const {boilerPlateCodes}=await Problems.findById(problemId)
+        const {slug,solution}=req.body
+        const propblem=await Problems.findOne({slug})
+        const {_id:problemId,boilerPlateCodes}=propblem
         if(!problemId||!solution.code||!solution.language){
             throw new Error("please ensure all feilds are filled")
         }
         const testCases=await TestCases.find({problem:problemId})
-        const {_id:submissionId}=await Submissions.create({problem:problemId,user:userId,language:solution.language,code:solution.code,timeForExecution:"null",status:"PENDING",memoryUsedInExecution:"null",testCasesRun:"null"})
+        const {_id:submissionId}=await Submissions.create({problem:problemId,user:userId,language:solution.language,code:solution.code,timeForExecution:0,status:"Pending",memoryUsedInExecution:0,testCasesPassed:0,totalTestCases:0})
+        res.status(201).send("code submitted sucessfully")
         const tokenstr=await verifyTestCases(testCases,solution,boilerPlateCodes)
-        const result=submitToken(tokenstr)
-        console.log({result})
+        const results=await submitToken(tokenstr)
+        const runInfo={}
+        for (const reasult of results){
+            console.log(reasult)
+            if(reasult.status.id==3){
+                runInfo.timeForExecution=runInfo.timeForExecution+Number(reasult.time) || Number(reasult.time)
+                runInfo.memory = (runInfo.memorty)?(Math.max(runInfo.memorty,reasult.memory)):(reasult.memory)
+            }
+            runInfo.status=reasult.status.description
+            if(reasult.status.id!=3){reasult=null}
+        }
+        console.log({runInfo})
+        const update={problem:problemId,user:userId,language:solution.language,code:solution.code,timeForExecution:runInfo.timeForExecution,status:runInfo.status,memoryUsedInExecution:runInfo.memory,testCasesPassed:results.length}
+        console.log({update})
+        await Submissions.findOneAndUpdate({_id:submissionId},update)
+        user.solved.includes(problemId)
 
-        await Submissions.findOneAndUpdate({_id:submissionId},{problem:problemId,user:userId,language:solution.language,code:solution.code,timeForExecution,status,memoryUsedInExecution,testCasesRun})
+
+        if (results.length==testCases.length && !user.solved.includes(problemId)){
+            const {solved}=user
+            solved.push(problemId)
+            await user.save()
+        }
 
     }
     catch(error){
@@ -27,3 +49,32 @@ export const submit=async (req,res)=>{
         res.status(500).send({msg:"an error occured",error:error.message||"an error occured",data:null})
     }
 }
+
+const runCode=async (req,res)=>{
+    try{
+        const {user} =req
+        const {_id:userId}=user
+        const {slug,solution}=req.body
+        const propblem=await Problems.findOne({slug})
+        const {_id:problemId,boilerPlateCodes}=propblem
+        if(!problemId||!solution.code||!solution.language){
+            throw new Error("please ensure all feilds are filled")
+        }
+        const testCases=await TestCases.find({problem:problemId})
+        const tokenstr=await verifyTestCases(testCases,solution,boilerPlateCodes)
+        const results=await submitToken(tokenstr)
+        for (let result of results){
+            if(result.status.id!=3){
+                return res.status(400).send(result.status.description)
+            }
+        }
+        res.status(200).send("code run sucessfully")
+    }
+    catch(error){
+        res.status(500).send({msg:"error occured",error:error.message||"unable to run your code"})
+    }
+}
+
+
+
+export {submit,runCode}
