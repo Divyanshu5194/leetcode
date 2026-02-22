ec718a7f4e1f89f93a221e61d0b0836e71776534
Divyanshu5194 <d40737865@gmail.com>
2026-02-22 13:35:14 +0530

    introduced authentication system and redirection to homepage if logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import Homepage from './pages/Homepage'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { checkAuth } from './features/auth/authSlice'
 
 function App() {
+  const {isAuthenticated}=useSelector((state)=>state.auth)
+  const dispatch=useDispatch()
+  console.log({isAuthenticated})
 
+  useEffect(()=>{dispatch(checkAuth())},[])
+  //useEffect(()=>{dispatch(refresh())})
   return (
     <>
       <Routes>
-        <Route path='/' element={<Homepage></Homepage>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/' element={ (isAuthenticated)?(<Homepage></Homepage>):(<Navigate to={"/signup"}></Navigate>)}></Route>
+        <Route path='/login' element={ (isAuthenticated)?(<Navigate to={"/"}></Navigate>):(<Login></Login>)}></Route>
+        <Route path='/signup' element={(isAuthenticated)?(<Navigate to={"/"}></Navigate>):(<Signup></Signup>)}></Route>
       </Routes>
     </>
   )
