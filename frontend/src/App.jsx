19e38d3b84d05e16167bdf43dacec14d5709df90
Divyanshu5194 <d40737865@gmail.com>
2026-03-06 19:36:16 +0530

    added routes for editingn problems,admin pannel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,12 @@ import { useEffect } from 'react'
 import { checkAuth, fetchLanguages } from './features/auth/authSlice'
 import Adminpage from './pages/adminpage'
 import ProblemPage from './pages/problempage'
+import CreateProblem from './pages/createProblem'
+import EditPage from './pages/editProblemPage'
 
 function App() {
   const {isAuthenticated,user}=useSelector((state)=>state.auth)
+  console.log({user})
   const dispatch=useDispatch()
   console.log({isAuthenticated})
 
@@ -20,12 +23,14 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={ (isAuthenticated)?(<Homepage></Homepage>):(<Navigate to={"/signup"}></Navigate>)}></Route>
+        <Route path='/' element={ (isAuthenticated)?(user.role=="Admin" ? <Navigate to={"/admin"}></Navigate> : <Homepage></Homepage>):(<Navigate to={"/signup"}></Navigate>)}></Route>
         <Route path='/login' element={ (isAuthenticated)?(<Navigate to={"/"}></Navigate>):(<Login></Login>)}></Route>
         <Route path='/signup' element={(isAuthenticated)?(<Navigate to={"/"}></Navigate>):(<Signup></Signup>)}></Route>
         <Route path='/signup' element={(isAuthenticated)?(<Navigate to={"/"}></Navigate>):(<Signup></Signup>)}></Route>
         <Route path='/problem/:slug' element={ (isAuthenticated)?(<ProblemPage></ProblemPage>):(<Navigate to={"/signup"}></Navigate>)}></Route>
-        {/* <Route path='/admin' element={<Adminpage></Adminpage>}></Route> */}
+        <Route path='/admin' element={(user && user.role=="Admin") ? <Adminpage></Adminpage> : <Navigate to={"/"}></Navigate>}></Route>
+        <Route path='/create' element={(user && user.role=="Admin") ? <CreateProblem></CreateProblem> : <Navigate to={"/"}></Navigate>}></Route>
+        <Route path='/problems/edit/:slug' element={(user && user.role=="Admin") ? <EditPage></EditPage> : <Navigate to={"/"}></Navigate>}></Route>
       </Routes>
     </>
   )
