5d62894246c515060a9f165121f5e55473f7e7e0
Divyanshu5194 <d40737865@gmail.com>
2026-03-07 19:25:28 +0530

    added feilds in problem page

--- frontend/src/pages/problempage.jsx
+++ frontend/src/pages/problempage.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import { axiosClient } from "../utils/axiosClient"
 import { useSelector } from "react-redux"
+import LanguageSelectorFeild from "../components/LanguageSelector"
+import Editor from "@monaco-editor/react";
+import { languages } from "monaco-editor"
 
 export default function ProblemPage(){
 
@@ -10,16 +13,39 @@ export default function ProblemPage(){
     const [problem,setProblem]=useState(null)
     const [error,setError]=useState(null)
     const [loading,setLoading]=useState(true)
-    const [value,setValue]=useState("")
-    const [language,setLanguage]=useState("select a language")
-
+    const [userCode,setUserCode]=useState("")
+    const [language,setLanguage]=useState(null)
+    const [codeEditorTheme,setCodeEditorTheme]=useState("vs-dark")
     const [runResponse,setRunResponse]=useState(null)
     const [runLoading,setRunLoading]=useState(false)
     const [runError,setRunError]=useState(null)
 
     console.log({languageArray})
 
+    
+
     useEffect(()=>{
+        if(languageArray){
+            setLanguage(languageArray[0])
+            const languageMap=[]
+
+            languageArray.forEach((languageObj)=>{
+                const Id=languageObj.id
+                const language=languageObj.name
+                const languageName=language.split(" ")[0]
+                const [_,...vArray]=language.split(" ")
+                let version=""
+                vArray.forEach((v,index)=>{if(index==0)return version+=v;return version+=` ${v}`})
+                console.log({languageName,version})
+                const existingLanguage=languageMap.find((language)=>language.name==languageName)
+                if(version){    if (!languageMap.includes(languageName) || existingLanguage.name.replace(languageName,"")<version){
+                        const name=languageName+" "+version;
+                        console.log({languageMapGoing:{id:Number(Id),name}})
+                        languageMap.push({id:Number(Id),name})
+                    }}
+            })
+            console.log({languageMap})
+        }
             (async ()=>{
             try{
                 const response=await axiosClient.get(`/Problems/${slug}`)
@@ -80,7 +106,7 @@ export default function ProblemPage(){
         <>
             <h2>{problem.title}</h2>
             <div>
-                <span>{problem.tags.map((tag)=><span className="bg-">{`${tag} `}</span>)}</span>
+                <span>{problem.tags.map((tag,index)=><span className="bg-" key={index+373}>{`${tag} `}</span>)}</span>
                 <span></span>
                 <span></span>
                 <span></span>
@@ -92,32 +118,34 @@ export default function ProblemPage(){
 
             <div>
                 <h3>Constratins</h3>
-                {problem.constraints.map((constraint)=><p>{constraint}</p>)}
+                {problem.constraints.map((constraint,index)=><p key={index+89754}>{constraint}</p>)}
             </div>
 
             <div>
                 <h3 className="text-bold">Follow Up Questions</h3>
                 {
-                    problem.followUpQuestions.map((followUp)=><><p>{followUp}</p><br></br></>)
+                    problem.followUpQuestions.map((followUp,index)=><div key={index+98}><p>{followUp}</p><br></br></div>)
                 }
             </div>
 
 
-            <div>
-
-                <select
-                    className="select select-bordered w-full max-w-xs" value={language} onChange={(e) => setLanguage(e.target.value)}>
-                        {
-                            languageArray.map((language)=><option key={language.id}>{language.name}</option>)
-                        }
-                    </select>
-            </div>
+            <LanguageSelectorFeild language={language} setLanguage={setLanguage}></LanguageSelectorFeild>
 
 
-            <div>
-                <p>Code</p>
-                <p>To initialize a project, use the <textarea onChange={(e)=>{setValue(e.target.value)}} value={value}></textarea> command.</p>
-            </div>
+            {/* <div style={{ height: "500px", border: "1px solid #ccc" }}>
+                <Editor
+                options={{
+                    fontSize: 20,
+                    padding: { top: 16, bottom: 16 },
+                }}
+                height="30vh"
+                width="60vw"
+                theme={codeEditorTheme} // now a valid theme ("vs", "vs-dark", "hc-black")
+                language={language.split(" ")[0] || "cpp"} // fallback to plaintext if unknown
+                value={userCode}
+                onChange={(value) => setUserCode(value)}
+                />
+            </div> */}
 
 
 
@@ -130,7 +158,8 @@ export default function ProblemPage(){
 
 
             {runLoading && <p className="text-red-500">running your code</p>}
-            {runError && <p className="text-red-500">{runError.data}</p>}
+            {runError && <p className="text-red-500">{runError.error}</p>}
+            {runResponse && <p className="text-red-500">{runResponse.data}</p>}
         </>
     )
 }
