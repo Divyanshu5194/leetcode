60d427ebc70428b29dc7f4099b934fa5801f47a3
Divyanshu5194 <d40737865@gmail.com>
2026-04-06 22:37:19 +0530

    fixed bugs

--- backend/controllers/video.controllers.js
+++ backend/controllers/video.controllers.js
@@ -3,9 +3,9 @@ import Problems from "../models/problems.models.js"
 import Video from "../models/video.models.js"
 
 cloudinary.config({
-    cloud_name:process.env.COULD_NAME,
+    cloud_name:process.env.CLOUD_NAME,
     api_key:process.env.API_KEY,
-    api_secret:process.env.API_SECRET
+    api_secret:process.env.API_SCERET
 })
 
 const getUploadSignature=async (req,res)=>{
@@ -22,11 +22,11 @@ const getUploadSignature=async (req,res)=>{
         const publicId=`leetcode-solutions/${problemId}/${userId}_${timestamp}`
 
         const uploadParams={
-            timestamp:timestamp,
-            public_id:publicId
+            public_id:publicId,
+            timestamp:timestamp
         }
 
-        const signature = cloudinary.utils.api_sign_request(uploadParams,process.env.API_SECRET)
+        const signature = cloudinary.utils.api_sign_request(uploadParams,process.env.API_SCERET)
 
         res.status(200).send({success:true,data:{
             signature,
@@ -34,7 +34,7 @@ const getUploadSignature=async (req,res)=>{
             public_id:publicId,
             api_key:process.env.API_KEY,
             cloud_name:process.env.COULD_NAME,
-            upload_url:`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/video/upload`,
+            upload_url:`https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/video/upload`,
         }})
     }
     catch{
@@ -44,16 +44,21 @@ const getUploadSignature=async (req,res)=>{
 }
 
 const saveMetaData=async (req,res)=>{
+    try{
     const {problemId,cloudinaryPublicId,cloudinaryUrl,secureUrl,duration}=req.body
     const {_id:userId}=req.user
 
     const cloudinaryResponse=await cloudinary.api.resource(cloudinaryPublicId,{resource_type:"video"})
 
+    console.log({
+        cloudinaryResponse
+    })
+
     if(!cloudinaryResponse){
         return res.status(400).send({success:false,error:"Video hasn't been uploaded"})
     }
 
-    const existingVideo=await Video.find({
+    const existingVideo=await Video.findOne({
         problem:problemId,
         user:userId,
         cloudinaryPublicId
@@ -83,40 +88,69 @@ const saveMetaData=async (req,res)=>{
         format:"mp4"
     })
 
-    const videoSolution=new Video({
+    const videoSolution=await Video.create({
         problem: problemId,
         createdBy: userId,
         publicId: cloudinaryResponse.public_id,
-        cloudinaryUrl: cloudinaryResponse.cloudinaryUrl,
-        secureUrl: cloudinaryResponse.secureUrl,
+        cloudinaryUrl: cloudinaryResponse.url,
+        secureUrl: cloudinaryResponse.secure_url,
         format: cloudinaryResponse.format,
         views:0,
-        likes:0,
-        durationInMilliseconds:cloudinaryResponse.duration,
-        size: cloudinaryResponse.size,
-        thumbnailUrl: thumbnailUrl,
+        durationInMilliseconds:duration,
+        size: cloudinaryResponse.bytes,
         })
+    
+        console.log("video uploaded",videoSolution)
 
-    Video.save()
+    return res.status(200).send({success:true,data:"Metadata Saved succesfully"})
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send({success:false,error:error.message || "An Error Occured"})
+    }
 }
 
 const deleteVideo=async (req,res)=>{
     const {problemId}=req.params
     //deletion from database
     try{
-        const solutionVideo=Video.findOneAndDelete({problem:problemId})
+        if(!problemId){
+            return res.status(400).send({success:false,error:"problem id not present"})
+        }
+        const solutionVideo=await Video.findOne({problem:problemId})
+        console.log({solutionVideo})
 
         if (!solutionVideo){
             return res.status(404).send({success:false,error:"Video to delete not found"})
         }
-        await cloudinary.uploader.destroy(solutionVideo.cloudinaryPublicId,{resource_type:"video",invalidate:true})
+        await Video.deleteOne({problem:problemId})
+        await cloudinary.uploader.destroy(solutionVideo.publicId,{resource_type:"video",invalidate:true})
         res.status(200).send({success:true,data:"Video Deletion Sucessful"})
     }
     catch(error){
+        console.log("error in deleting video",error)
         return res.status(500).send({success:false,error:"encountered a error in deleting the video"})
+    }
+}
+
+const checkExistingVideo=async (req,res)=>{
+    try{
+        const {problemId}=req.params
+        const existingVideo=await Video.findOne({problem:problemId})
+
+        console.log({existingVideo})
+
+        if(existingVideo){
+            return res.status(200).send({success:true,data:{secureUrl:existingVideo.secureUrl,duration:existingVideo.durationInMilliseconds}})
+        }
+        res.status(404).send({success:true,data:"Video Not Found"})
+    }
+    catch(error){
+        console.log("error in checking video",error)
+        res.status(500).send({success:true,error:error.message || "Error Serving Request"})
     }
 }
 
 
 
-export {getUploadSignature,saveMetaData,deleteVideo}
+export {getUploadSignature,saveMetaData,deleteVideo,checkExistingVideo}
