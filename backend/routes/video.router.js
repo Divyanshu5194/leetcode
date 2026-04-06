049110cfd330e9139f4ae594642771f5947ea43a
Divyanshu5194 <d40737865@gmail.com>
2026-04-06 22:38:05 +0530

    added a route to fetch url of existing video

--- backend/routes/video.router.js
+++ backend/routes/video.router.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { userAuth } from "../middlewares/auth.js"
 import { adminMiddleware } from "../middlewares/adminMiddleware.js"
-import { deleteVideo, getUploadSignature, saveMetaData } from "../controllers/video.controllers.js"
+import { checkExistingVideo, deleteVideo, getUploadSignature, saveMetaData } from "../controllers/video.controllers.js"
 
 
 const videoRouter=express.Router()
@@ -9,6 +9,9 @@ const videoRouter=express.Router()
 //get Digital Singature
 
 videoRouter.get("/getDigitalSignature/:problemId",adminMiddleware,getUploadSignature)
+
+//check if video exists
+videoRouter.get("/checkVideoExistannce/:problemId",userAuth,checkExistingVideo)
 
 
 //save Meta Data
